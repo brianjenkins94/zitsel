940c2a3f6c38e882cc604e3b987b2dacfbd92916
Brian Jenkins <brianjenkins94@gmail.com>
2023-06-15 11:40:42 -0400

    Update nodebox.js

--- public/js/nodebox.js
+++ public/js/nodebox.js
@@ -3,9 +3,11 @@ import { Nodebox } from "https://cdn.jsdelivr.net/npm/@codesandbox/nodebox/build
 const nodeboxIframe = document.getElementById("nodebox-iframe");
 const previewIframe = document.getElementById("preview-iframe");
 
+const BASE_URL = location.pathname;
+
 const nodebox = new Nodebox({
 	"iframe": nodeboxIframe,
-	"runtimeUrl": "/vendor/codesandbox/bridge.html"
+	"runtimeUrl": BASE_URL + "vendor/codesandbox/bridge.html"
 });
 
 await nodebox.connect();
@@ -18,4 +20,4 @@ const { id } = await shell.runCommand("node", ["index.js"]);
 
 const { url } = await nodebox.preview.getByShellId(id);
 
-previewIframe.setAttribute("src", "/vendor/codesandbox/preview.html");
+previewIframe.setAttribute("src", BASE_URL + "/vendor/codesandbox/preview.html");
