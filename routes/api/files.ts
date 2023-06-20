1e7bb2a83695b3d54bca79caddb06d9cb9bfbed4
Brian Jenkins <brianjenkins94@gmail.com>
2023-06-20 16:06:52 -0400

    Remove unused

--- routes/api/files.ts
+++ routes/api/files.ts
@@ -29,7 +29,6 @@ export async function get(request, response) {
 			import * as path from "path";
 			import * as url from "url";
 
-			const location = globalThis?.location ?? new URL("http://localhost:8000/TypeCraft/proxy/worker.js");
 			const BASE_URL = "http://localhost:8000";
 			const BASE_PATH = location.href.substring(location.origin.length, location.href.lastIndexOf("/"));
 
